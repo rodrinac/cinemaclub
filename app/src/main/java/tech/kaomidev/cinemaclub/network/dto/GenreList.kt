f9fd577f2574f91771c29c7e3138e4d506d56356
Rodrigo <jose.rs.inacio@gmail.com>
2020-06-05 22:56:26 -0300

    Initial commit

--- app/src/main/java/tech/kaomidev/cinemaclub/network/dto/GenreList.kt
+++
@@ -1,5 +0,0 @@
-package tech.kaomidev.cinemaclub.network.dto
-
-class GenreList {
-    var genres: List<GenreDto>? = null
-}
--- app/src/main/java/tech/kaomidev/cinemaclub/network/dto/ResultPage.kt
+++
@@ -1,8 +0,0 @@
-package tech.kaomidev.cinemaclub.network.dto
-
-class ResultPage<T> {
-    @Suppress("unused")
-    var page: Long? = null
-    var results: List<T>? = null
-}
-
